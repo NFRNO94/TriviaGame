400e36def1cd3d021a665f59c30a7a28319b786a
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-03-21 17:13:29 -0700

    this is my fourth commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,31 @@
 $(document).ready(function () {
-    var display = ("#game-space");
-    var counterStart = 30;
 
+    // create on click events
+    ///////////////////////////////////////////////////////////////////////
+    //start game
+    $("#start-game").on("click", gameStart());
+
+    //reset game
+    $("#reset-game").on("click", reset());
+
+    //next question after selecting an answer
+    $("#user-answer").on("click", nextQuestion());
+
+    ////////////////////////////////////////////////////////////////////////
+
+    // create var to store the start timer number of 30 seconds, and set countdown to not running
+    var counterTime = 30;
+    var clockCountdown = false;
+
+    //var to store right answers, wrong, and unanswered
+    var rightAnswer = 0;
+    var wrongAnswer = 0;
+    var unanswered = 0;
+    
+
+    // create an array of questions to be stored in objects
     var questionsArray = [{
-        question: "Who was the lead singer of the famous 90's band Nirvana?",
+        question: "Who was the lead singer of the famous 90's rock band Nirvana?",
         answers: ["Kurt Cobain", "Ringo Star", "Adam Gontier", "Steven Tyler"],
         correctAnswer: "Kurt Cobain",
         image: "assets/images/kurt-cobain.gif"
@@ -51,15 +73,48 @@ $(document).ready(function () {
         image: "assets/images/ivan-moody.gif"
     }];
 
-    var gameObj = {
-        currentQuestion: 0,
-        questions: questionsArray,
-        counter:  counterStart,
-        correctAnswer: 0,
-        incorrectAnswer: 0,
-        timerCountdown: function() {
-            gameObj.counter--;
+    //create var to store game start function
+function gameStart() {
 
-        }
+}
+// create countdown timer to include in gameStart function
+function countdown() {
 
-    }
+}
+//load question function 
+function loadQuestion() {
+
+}
+//display next question
+function nextQuestion() {
+
+}
+
+
+//create function for time up
+function timeUp() {
+    //setTimeout to 30 seconds
+    setTimeout(timeUp, 1000 * 30);
+}
+
+//correct answer function
+function answerCorrect() {
+
+}
+
+//incorrect answer function
+function answerIncorrect() {
+
+}
+
+//display results function
+function results() {
+
+}
+
+//reset game function
+function reset() {
+
+}
+
+})
